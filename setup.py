d985cdb37861c0765eb8b262d9b17dcb8edc36b9
Abhishek Nandi <abhishekapdj01@gmail.com>
2024-09-26 11:16:37 +0530

    Your descriptive commit message here

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='scrapper',
+    version='0.0.1',
+    author='Nandi',
+    author_email='abhishekapdj01@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
